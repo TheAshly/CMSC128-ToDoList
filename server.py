986b7924031142c371bdef84ed1f4eff0cfe66e3
Christian Jave S. Hulleza <hullezachristianjave@gmail.com>
2025-09-26 17:29:30 +0800

    Added Fetching of Data from DB to HTML

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, redirect, render_template, request
 import sqlite3
 from datetime import datetime
+import json
 
 # Database 
 conn = sqlite3.connect("tasks.db", check_same_thread=False)
+conn.row_factory = sqlite3.Row
 cursor = conn.cursor()
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS tasks (       
     id          INTEGER PRIMARY KEY AUTOINCREMENT,            
@@ -22,36 +25,26 @@ def db_add_task (name, priority, date_due, time_due, date_now, time_now):
     cursor.execute("""
     INSERT INTO tasks (name, priority, date_added, time_added, date_due, time_due, completed)
     VALUES (?, ?, ?, ? ,?, ?, ?)
-    """, (name, priority, date_due, time_due, date_now, time_now, 0))
+    """, (name, priority, date_now, time_now, date_due, time_due, 0))
     conn.commit()
-
     print(f"Task {name} has been added!")
 
 def db_clear_rows(): 
-    cursor.execute("""
-    DELETE from tasks
-    """)
+    cursor.execute("""DELETE from tasks""")
     conn.commit()
-
-    db_get_rows()
-
 
 def db_get_rows():
     cursor.execute("SELECT * FROM tasks")
-    tasks = cursor.fetchall()
-    
-    if (len(tasks) == 0): print("None!") 
-    for task in tasks:
-        print(task)
-
-
+    rows = cursor.fetchall()
+    tasks = [dict(row) for row in rows]
+    return tasks
 
 # Server  
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", tasks=db_get_rows())
 
 
 @app.route('/add-task', methods=['post'])
@@ -66,9 +59,8 @@ def add_task():
     time_now = now.strftime("%H:%M")
 
     db_add_task(name, priority, date_due, time_due, date_now, time_now)
-    db_get_rows()
-
     return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
